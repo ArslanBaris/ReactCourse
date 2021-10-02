8e59d469d05cd1d4ed192cfbd9c6cf5c97687424
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-02 21:07:25 +0300

    refactor: App.js revised.

--- intro/src/App.js
+++ intro/src/App.js
@@ -5,7 +5,7 @@ import Navi from "./Navi";
 import ProductList from "./ProductList";
 
 export default class App extends Component {
-  state = { currentCategory: "", products: [] };
+  state = { currentCategory: "", products: [], cart:[] };
   changeCategory = (category) => {
     this.setState({ currentCategory: category.categoryName });
     this.getProducts(category.id)
@@ -26,16 +26,27 @@ export default class App extends Component {
       .then((data) => this.setState({ products: data }));
   };
 
+  addToCart =(product)=>{
+   let newCart = this.state.cart;
+   var addedItem = newCart.find(c=>c.product.id===product.id);
+   if(addedItem){
+     addedItem.quantity+=1;
+   }
+   else{
+    newCart.push({product:product,quantity:1});
+    this.setState({cart:newCart});
+   }
+  
+}
+
   render() {
     let productInfo = { title: "ProductList" };
     let categoryInfo = { title: "CategoryList" };
 
     return (
       <div>
-        <Container>
-          <Row>
-            <Navi />
-          </Row>
+        <Container>            
+            <Navi cart={this.state.cart} />         
           <Row>
             <Col xs="3">
               <CategoryList
@@ -47,6 +58,7 @@ export default class App extends Component {
             <Col xs="9">
               <ProductList
                 currentCategory={this.state.currentCategory}
+                addToCart={this.addToCart}
                 info={productInfo}
                 products={this.state.products}
               />
