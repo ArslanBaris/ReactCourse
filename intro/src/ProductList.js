491129f36785abaca3185860237468c1052c347d
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-02 16:08:59 +0300

    refactor: ProductList.js revised.

--- intro/src/ProductList.js
+++ intro/src/ProductList.js
@@ -4,7 +4,7 @@ export default class ProductList extends Component {
     render() {
         return (
             <div>
-                <h2>Product List</h2>
+                <h3>{this.props.title}</h3>
             </div>
         )
     }
