3dfd4e5dedcb6e2d71f31930fbca53d472d768a1
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-02 16:08:32 +0300

    refactor: App.js revised.

--- intro/src/App.js
+++ intro/src/App.js
@@ -5,6 +5,8 @@ import Navi from "./Navi";
 import ProductList from "./ProductList";
 
 function App() {
+  let titleProduct="Product List";
+  let titleCategory="Category List";
   return (
     <div>
       <Container>
@@ -13,10 +15,10 @@ function App() {
         </Row>
         <Row>
           <Col xs="3">
-            <CategoryList />
+            <CategoryList title ={titleCategory} />
           </Col>
           <Col xs="9">
-            <ProductList />
+            <ProductList title ={titleProduct}/>
           </Col>
         </Row>
       </Container>
