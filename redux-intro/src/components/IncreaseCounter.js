c31aac7c7e36743c87aa3d3fcd4faf342d999dbc
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-20 21:34:07 +0300

    IncreaseCounter added

---
+++ redux-intro/src/components/IncreaseCounter.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import {increaseCounter} from "../redux/actions/counterActions";
+
+class IncreaseCounter extends Component {
+  render() {
+    return (<div>
+        <button onClick={e=>{
+            this.props.dispatch(increaseCounter());
+        }}>1 arttır</button>
+    </div>);
+  }
+}
+
+function mapDispatchToPros(dispatch) {
+  return { action: bindActionCreators(increaseCounter, dispatch) };
+}
+
+export default connect(mapDispatchToPros)(IncreaseCounter);
